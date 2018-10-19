bbfc3a68a777db3647c713a90aef09bb5a639d34
GoremanIX <jansen.arthan@gmail.com>
2018-10-19 13:25:59 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Knghtside
---
+++ desktop/src/me/arthan/knightside/desktop/DesktopLauncher.kt
@@ -0,0 +1,13 @@
+package me.arthan.knightside.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import me.arthan.knightside.Knightside;
+
+class DesktopLauncher {
+
+}
+fun main (args: Array<String>) {
+    var config = LwjglApplicationConfiguration()
+    LwjglApplication(Knightside(), config);
+}
