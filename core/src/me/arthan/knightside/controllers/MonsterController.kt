a8663d4c5404dd14f72d11e49fb21ddfff7ac67f
OisinA <oisinaylward@gmail.com>
2018-10-30 13:41:22 +0000

    Multiply by pi.

--- core/src/me/arthan/knightside/controllers/MonsterController.kt
+++ core/src/me/arthan/knightside/controllers/MonsterController.kt
@@ -5,12 +5,13 @@ import me.arthan.knightside.models.Map
 import me.arthan.knightside.models.entity.Entity
 import me.arthan.knightside.models.entity.monster.Monster
 import java.util.*
+import kotlin.math.PI
 
 class MonsterController(val monster: Monster): Controller() {
 
     val rand = Random()
 
-    val dirs: DoubleArray = doubleArrayOf(0.0, 0.5, 1.0, 1.5)
+    val dirs: DoubleArray = doubleArrayOf(0.0 * PI, 0.5 * PI, 1.0 * PI, 1.5 * PI)
 
     override fun update(delta: Float, map: Map, entities: ArrayList<Entity>) {
         if(rand.nextInt(100) == 4) {
